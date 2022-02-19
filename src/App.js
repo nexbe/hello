1001bc60e5bc39e2b1fca4aa6b7b614f88759cb6
Ken Ling <ken88ling@gmail.com>
2022-02-19 23:57:26 +0800

    Update App.js
    set state
    on event handle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header';
 
 function App() {
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  function handleClick() {
+    setfirstName('');
+    setLastName('');
+    console.log('button click');
+  }
+
+  function handleFirstNameChange(e) {
+    setfirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
   return (
     <div className='container'>
       <div className='header'>
         <Header />
       </div>
-      <div className='content'>New Content</div>
+      <div className='content'>
+        <input
+          type='text'
+          onChange={handleFirstNameChange}
+          placeholder='Please key first Name'
+        />
+        <br />
+        <input
+          type='text'
+          onChange={handleLastNameChange}
+          placeholder='Please key last Name'
+        />
+        <br />
+        firstName : {firstName}
+        <br />
+        last Name : {lastName}
+        <hr />
+      </div>
       <div className='footer'>New Footer</div>
     </div>
   );
